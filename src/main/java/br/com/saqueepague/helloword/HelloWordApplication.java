f5365b1653741686034b371a6ea3ee804e800295
Maurício Maslowski Pedroso <mauricio.pedroso@saqueepague.com.br>
2024-07-08 19:01:44 -0300

    Teste senha

--- src/main/java/br/com/saqueepague/helloword/HelloWordApplication.java
+++ src/main/java/br/com/saqueepague/helloword/HelloWordApplication.java
@@ -1,12 +1,21 @@
 package br.com.saqueepague.helloword;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HelloWordApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWordApplication.class);
+
     public static void main(String[] args) {
+        LOG.debug("Senha: {}", args[0]);
+        LOG.debug("Senha: {}", "Teste@123");
+        LOG.debug("Password: {}", args[0]);
+        LOG.debug("Password: {}", "Teste@123");
+
         SpringApplication.run(HelloWordApplication.class, args);
     }
 
